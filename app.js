b2816c8a8ef2f5f76a97ea559ee76d7d4d9c2b39
ngantn1994 <ngantn1994@gmail.com>
2021-09-03 23:00:23 +0900

    init simple Vue project

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.VUE_APP_BACK_END_PORT;
 
 app.get('/', (req, res) => {
   axios.get('http://api.openweathermap.org/data/2.5/weather?q=Hanoi&appid=' + process.env.OPEN_WEATHER_APP_ID)
